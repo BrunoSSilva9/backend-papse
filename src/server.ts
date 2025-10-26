b06334c3ec249b80578b0fc79b05e19b905a65e5
BrunoSSilva9 <brunos.ss2002@gmail.com>
2025-10-26 00:33:47 -0300

    import da rota listEspera

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import pacienteRoutes from "./routes/paciente.routes.js";
 import protocoloRoutes from "./routes/protocolo.routes.js";
 import colaboradorRoutes from "./routes/colaborador.routes.js";
+import listaEsperaRoutes from "./routes/listaEspera.routes.js";
 
 console.log("✅ server.ts foi carregado");
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use("/api", pacienteRoutes);
 app.use("/api", protocoloRoutes);
 app.use("/api", colaboradorRoutes);
+app.use("/api", listaEsperaRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "API está funcionando!" });
