10d0383928990e8c256fe11efd88a231e72c7e94
AndreSantosDS <andre8628852@gmail.com>
2025-10-28 20:35:56 -0300

    feat: crud criado da tabela Regular

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import protocoloRoutes from "./routes/protocolo.routes.js";
 import colaboradorRoutes from "./routes/colaborador.routes.js";
 import listaEsperaRoutes from "./routes/listaEspera.routes.js";
 import listaRegularRoutes from "./routes/listaRegular.routes.js";
+import regularRoutes from "./routes/regular.routes.js";
 
 console.log("✅ server.ts foi carregado");
 
@@ -16,7 +17,7 @@ app.use("/api", pacienteRoutes);
 app.use("/api", protocoloRoutes);
 app.use("/api", colaboradorRoutes);
 app.use("/api", listaEsperaRoutes);
-app.use("/api", listaRegularRoutes);
+app.use("/api", regularRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "API está funcionando!" });
