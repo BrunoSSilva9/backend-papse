29f1d34f8a14b18c30aa7a1ebb2a85ae9916b8ea
BrunoSSilva9 <brunos.ss2002@gmail.com>
2025-12-15 00:05:33 -0300

    update about text

--- front-react-papse/src/pages/About.jsx
+++ front-react-papse/src/pages/About.jsx
@@ -16,12 +16,9 @@ const About = () => {
                     </div>
 
                     <p className={styles.paragraphSobre}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget faucibus lectus, nec gravida eros. Sed vel fermentum leo, nec interdum eros.
-                        Nullam vel suscipit ligula, at interdum leo. Etiam non tempus neque. Quisque molestie hendrerit massa ornare tristique.
-                        Pellentesque molestie nisl felis. Morbi at molestie massa, non malesuada orci. Vestibulum iaculis condimentum nunc, eget pretium orci volutpat sed.
-                        Fusce consequat orci eu pulvinar rhoncus.
-                        Cras sit amet euismod elit. Mauris vestibulum sapien lobortis, posuere ante et, ultricies est. Phasellus ex metus, suscipit a enim id, commodo commodo arcu.
-                        Pellentesque vitae nunc mi. Vivamus id tortor fermentum turpis faucibus porta eget vitae nunc. Maecenas ultricies tellus risus, sed luctus nunc luctus vitae.
+                        O Programa de Acompanhamento Psicológico Estudantil (PAPSE) teve início em 2019 e foi criado com intuito de promover atendimento psicológico aos estudantes da Unifesspa. Estes atendimentos são feitos por discentes regularmente matriculados no Curso de Psicologia, com supervisão clínica, tendo como base a Teoria Psicanalítica. As sessões normalmente acontecem uma vez por semana, de forma
+                        presencial ou remota. As inscrições são realizadas pelo e-mail agendapsico@unifesspa.edu.br. Os principais objetivos do programa são: atender à demanda de atendimentos psicológicos de discentes da Unifesspa; oferecer supervisão clínica, colaborando na formação dos discentes de Psicologia selecionados no Programa;
+                        e contribuir para redução da taxa de evasão/retenção entre os discentes da Unifesspa, ocasionadas por problemas psicológicos. O grupo está atualmente composto por seis discentes bolsistas, quatro discentes voluntárias, uma técnica colaboradora e uma supervisora, docente na mesma faculdade (FAPSI). No ano de 2024, já foram atendidos 96 discentes. Contudo, desde o início do programa, mais de 300 estudantes de diversos cursos da Unifesspa já se beneficiaram.  O PAPSE também oferece um atendimento de protocolo, em que são realizadas até cinco sessões, das quais, após serem concluídas, quem desejar segue aguardando disponibilidade de vaga para atendimento regular na fila de espera.
                     </p>
                 </main>
             </div>
